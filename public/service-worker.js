a0fdd17890a1f6ca6579dc2d160ced467e009b67
alexlockhart7 <alex.lockhart@icfolson.com>
2018-08-11 14:22:24 -0400

    Added a basic service worker.

---
+++ public/service-worker.js
@@ -0,0 +1,25 @@
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime-v1';
+
+const PRECACHE_URLS = [ ];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
